f9db7091eb6ecafbca74728bd8e00e2f9e4345fe
Taehwi <paul960801@gmail.com>
2025-09-29 16:14:29 +0900

    init commit

---
+++ goatgam/src/main/java/com/sparta/goatgam/GoatgamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.goatgam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatgamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoatgamApplication.class, args);
+    }
+
+}
