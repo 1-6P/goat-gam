c6334d168906295c6acd3b42a4970c38882e2eca
Taehwi <paul960801@gmail.com>
2025-09-30 16:01:46 +0900

    feat : swagger 설정 일부 추가

--- goatgam/src/main/java/com/sparta/goatgam/config/WebSecurityConfig.java
+++ goatgam/src/main/java/com/sparta/goatgam/config/WebSecurityConfig.java
@@ -40,6 +40,8 @@ public class WebSecurityConfig {
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
         JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        // 로그인 URL
+        filter.setFilterProcessesUrl("/api/v1/login");
         filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
         return filter;
     }
@@ -63,14 +65,16 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers(
+                                "/api/v1/auth/**",  // '/api/v1/auth'로 시작하는 요청 모두 접근 허가
+                                "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                        .permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
-        http.formLogin((formLogin) ->
-                formLogin
-                        .loginPage("/api/user/login-page").permitAll()
-        );
+        // 폼 로그인 / 로그아웃 비활성화
+        http.formLogin((form) -> form.disable()).logout(lo -> lo.disable());
+
 
         // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
--- goatgam/src/main/java/com/sparta/goatgam/controller/UserController.java
+++ goatgam/src/main/java/com/sparta/goatgam/controller/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
