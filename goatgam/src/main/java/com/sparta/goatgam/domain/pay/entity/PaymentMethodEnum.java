1139917887e6553c3e8dcff0d8db9ba824c3c80b
River is line <dmstjs7047@hanmail.net>
2025-10-02 21:51:55 +0900

    feat : 결제 테이블 Entity 생성
    토스페이먼트의 가이드를 참고. 결제 방법의 경우 확장성을 고려하여 카드 이외의 결제 방법을 추가함.

---
+++ goatgam/src/main/java/com/sparta/goatgam/domain/pay/entity/PaymentMethodEnum.java
@@ -0,0 +1,8 @@
+package com.sparta.goatgam.domain.pay.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentMethodEnum {
+    Card, Cash, InPerson
+}
---
+++ goatgam/src/main/java/com/sparta/goatgam/domain/pay/entity/paymentStatusEnum.java
@@ -0,0 +1,5 @@
+package com.sparta.goatgam.domain.pay.entity;
+
+public enum paymentStatusEnum {
+    Done, Canceled, Failed, Refunded
+}
