e2427d947043b1b842b4ff23419d1502c632d955
hangw2130\hangw <hangw2130@gmail.com>
2025-10-02 15:38:33 +0900

    메뉴 삭제 기능 구현

--- goatgam/src/main/java/com/sparta/goatgam/domain/owner/controller/FoodController.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/owner/controller/FoodController.java
@@ -4,6 +4,7 @@ import com.sparta.goatgam.domain.owner.dto.FoodRequestDto;
 import com.sparta.goatgam.domain.owner.dto.FoodResponseDto;
 import com.sparta.goatgam.domain.owner.repository.FoodRepository;
 import com.sparta.goatgam.domain.owner.service.FoodService;
+import com.sparta.goatgam.domain.user.entity.User;
 import com.sparta.goatgam.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -51,5 +52,22 @@ public class FoodController {
 
 
         return foodService.updateFood(restaurantId, menuId, foodRequestDto);
+    }
+
+    @DeleteMapping("/{menuId}")
+    public FoodResponseDto deleteFood(@PathVariable UUID restaurantId,
+                                      @PathVariable UUID menuId,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+//        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+//                .orElseThrow(() -> new IllegalArgumentException("해당 식당이 없습니다."));
+//
+        User currentUser = userDetails.getUser();
+//
+//        if (!restaurant.getOwner().getId().equals(currentUser.getId())) {
+//            throw new AccessDeniedException("해당 식당에 대한 권한이 없습니다.");
+//        }
+
+        return foodService.deleteFood(restaurantId, menuId, currentUser.getNickname());
     }
 }
--- goatgam/src/main/java/com/sparta/goatgam/domain/owner/service/FoodService.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/owner/service/FoodService.java
@@ -17,6 +17,7 @@ public class FoodService {
     private final FoodRepository foodRepository;
 //    private final RestaurantRepository restaurantRepository;
 
+    @Transactional
     public FoodResponseDto addFood(UUID restaurantId, FoodRequestDto foodRequestDto) {
 //        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(() -> new IllegalArgumentException("Restaurant not found"));
 
@@ -24,22 +25,41 @@ public class FoodService {
                 .foodPrice(foodRequestDto.getPrice())
                 .foodImage(foodRequestDto.getImage())
                 .foodExplain(foodRequestDto.getExplain())
-                .foodStatus(FoodStatus.valueOf(foodRequestDto.getStatus())).build();
-
-
+                .foodStatus(FoodStatus.valueOf(foodRequestDto.getStatus()))
+//                .restaurant(restaurant)
+                .build();
 
         UUID id = foodRepository.save(food).getId();
 
         return new FoodResponseDto("success", id);
     }
 
+    @Transactional
     public FoodResponseDto updateFood(UUID restaurantId, UUID menuId, FoodRequestDto foodRequestDto) {
         Food food = foodRepository.findById(menuId).orElseThrow(() -> new RuntimeException("해당 음식이 없습니다."));
 
+//        if (!food.getRestaurant().getId().equals(restaurantId)) {
+//            throw new RuntimeException("이 메뉴는 해당 식당의 메뉴가 아닙니다.");
+//        }
+
         food.update(foodRequestDto);
 
-        foodRepository.save(food);
         return new FoodResponseDto("success", food.getId());
+    }
+
+    @Transactional
+    public FoodResponseDto deleteFood(UUID restaurantId, UUID menuId, String userNickname) {
+//        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(() -> new IllegalArgumentException("Restaurant not found"));
+        Food food = foodRepository.findById(menuId).orElseThrow(() -> new RuntimeException("해당 음식이 없습니다."));
+
+//        if (!food.getRestaurant().getId().equals(restaurantId)) {
+//            throw new RuntimeException("이 메뉴는 해당 식당의 메뉴가 아닙니다.");
+//        }
+
+        food.changeStatus(FoodStatus.Deleted);
+        food.deleted(userNickname);
+
+        return new FoodResponseDto("success", menuId);
     }
 }
 
