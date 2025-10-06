50af2da2d52857c3113dc79eea64b09cd199fc1d
jinhyeon <jingoo211@gmail.com>
2025-10-06 20:42:48 +0900

    merge

---
+++ goatgam/src/main/java/com/sparta/goatgam/domain/restaurant/dto/RestaurantFoodOptionDetailDto.java
@@ -0,0 +1,30 @@
+package com.sparta.goatgam.domain.restaurant.dto;
+
+import com.sparta.goatgam.domain.owner.entity.FoodOption;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Getter
+@AllArgsConstructor
+public class RestaurantFoodOptionDetailDto {
+    private UUID restaurantId;
+    private UUID foodId;
+    private UUID optionId;
+    private String optionContents;
+    private int optionSurcharge;
+
+    public static List<RestaurantFoodOptionDetailDto> convertList(UUID restaurantId, UUID foodId ,List<FoodOption> foodOption) {
+        return foodOption.stream().
+                map(fo -> new RestaurantFoodOptionDetailDto(
+                        restaurantId,
+                        foodId,
+                        fo.getId(),
+                        fo.getContents(),
+                        fo.getSurcharge()
+                )).toList();
+    }
+}
