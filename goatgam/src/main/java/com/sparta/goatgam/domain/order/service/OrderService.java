471c330da55265c2f691d993adbfa2550b30e10d
River is line <dmstjs7047@hanmail.net>
2025-10-08 04:31:33 +0900

    refactor : pageable 객체를 반환하는 함수 분리
    목록을 조회하는 함수가 모두 페이지네이션을 사용하여 중복되는 코드를 분리함.

--- goatgam/src/main/java/com/sparta/goatgam/domain/order/service/OrderService.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.sparta.goatgam.domain.order.service;
 
+import com.sparta.goatgam.domain.order.dto.AdminOrderSummaryResponseDto;
 import com.sparta.goatgam.domain.order.dto.OrderSummaryResponseDto;
 import com.sparta.goatgam.domain.order.repository.OrderRepository;
 import com.sparta.goatgam.domain.user.entity.User;
@@ -22,7 +23,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     public PagedModel<OrderSummaryResponseDto> getMyOrderSummary(int page, int size, User user) {
-        Pageable pageable = makePageable(page, size);
+        Pageable pageable = makePageable(page, size, order(Sort.Direction.DESC, "createdAt"));
 
         Page<OrderSummaryResponseDto> orderSummaryList =
                 orderRepository.findAllByUser(user, pageable).map(OrderSummaryResponseDto::new);
@@ -30,28 +31,36 @@ public class OrderService {
         return new PagedModel<>(orderSummaryList);
     }
 
-    public PagedModel<OrderSummaryResponseDto> getUserOrderSummary(Long userId, int page, int size) {
-        Pageable pageable = makePageable(page, size);
+    public PagedModel<AdminOrderSummaryResponseDto> getUserOrderSummary(Long userId, int page, int size) {
+        Pageable pageable = makePageable(page, size,
+                order(Sort.Direction.ASC, "userUserId"),
+                order(Sort.Direction.DESC, "createdAt")
+        );
 
-        Page<OrderSummaryResponseDto> orderSummaryList;
+        Page<AdminOrderSummaryResponseDto> orderSummaryList;
 
         if (userId != null) {
             User user = userRepository.findById(userId)
                     .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다. id=" + userId));
 
-            orderSummaryList = orderRepository.findAllByUser(user, pageable).map(OrderSummaryResponseDto::new);
+            orderSummaryList = orderRepository.findAllByUser(user, pageable).map(AdminOrderSummaryResponseDto::new);
         } else {
-            orderSummaryList = orderRepository.findAll(pageable).map(OrderSummaryResponseDto::new);
+            orderSummaryList = orderRepository.findAll(pageable).map(AdminOrderSummaryResponseDto::new);
         }
 
         return new PagedModel<>(orderSummaryList);
     }
 
-    private Pageable makePageable(int page, int size) {
+
+    private Pageable makePageable(int page, int size, Sort.Order... orders) {
         if (size != 10 && size != 30 && size != 50) size = 10;
 
-        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        Sort sort = Sort.by(orders);
 
         return PageRequest.of(page, size, sort);
+    }
+
+    private Sort.Order order(Sort.Direction direction, String field) {
+        return new Sort.Order(direction, field);
     }
 }
