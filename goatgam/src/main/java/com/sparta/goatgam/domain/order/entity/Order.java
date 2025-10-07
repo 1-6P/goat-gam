ba12421606b3054d9ee7f7c7ae5a6ef720378ea9
River is line <dmstjs7047@hanmail.net>
2025-10-08 03:02:47 +0900

    feat : OrderFoodEntity와 양방향 연결

--- goatgam/src/main/java/com/sparta/goatgam/domain/order/entity/Order.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/order/entity/Order.java
@@ -8,6 +8,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -37,10 +38,10 @@ public class Order extends BaseEntity {
     private LocalDateTime orderTime;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false, updatable = false, length = 10)
+    @Column(name = "status", nullable = false, length = 10)
     private StatusEnum status;
 
-    @Column(name = "status_by", nullable = false, updatable = false, length = 50)
+    @Column(name = "status_by", nullable = false, length = 50)
     private String statusBy;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -50,4 +51,7 @@ public class Order extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderFood> orderFoods;
 }
