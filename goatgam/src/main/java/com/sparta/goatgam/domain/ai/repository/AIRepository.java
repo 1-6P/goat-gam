4346063170fef77b3348f6a6ab9606905c592410
hangw2130\hangw <hangw2130@gmail.com>
2025-10-08 16:05:18 +0900

    AI entity, repository 생성

---
+++ goatgam/src/main/java/com/sparta/goatgam/domain/ai/repository/AIRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.goatgam.domain.ai.repository;
+
+import com.sparta.goatgam.domain.ai.entity.AI;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AIRepository extends JpaRepository<AI, Integer> {
+}
