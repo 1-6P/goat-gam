7e460d8a52662da3b64c38e6a49bf952e0a57d0e
hangw2130\hangw <hangw2130@gmail.com>
2025-10-09 19:59:59 +0900

    repository 수정

--- goatgam/src/main/java/com/sparta/goatgam/domain/ai/repository/AIRepository.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/ai/repository/AIRepository.java
@@ -3,5 +3,7 @@ package com.sparta.goatgam.domain.ai.repository;
 import com.sparta.goatgam.domain.ai.entity.AI;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AIRepository extends JpaRepository<AI, Integer> {
+import java.util.UUID;
+
+public interface AIRepository extends JpaRepository<AI, UUID> {
 }
