6ab2a1cb95aecc784d8dc2cb9330484290fe7af5
JungInGyu <dltnswn20@naver.com>
2025-10-14 11:54:28 +0900

    refactor : 스웨거 tag 및 operation 추가

--- goatgam/src/main/java/com/sparta/goatgam/domain/review/controller/ReviewController.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/review/controller/ReviewController.java
@@ -6,6 +6,8 @@ import com.sparta.goatgam.domain.review.dto.ReviewUpdateResponseDto;
 import com.sparta.goatgam.domain.review.dto.UpdateReviewRequestDto;
 import com.sparta.goatgam.domain.review.service.ReviewService;
 import com.sparta.goatgam.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -19,10 +21,12 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Tag(name = "후기 API", description = "후기 관련 기능 API입니다.")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @Operation(summary = "후기 작성", description = "배송이 완료된 주문일 경우 후기를 작성합니다.")
     @PostMapping("/review/{restaurantId}/{orderId}")
     public ResponseEntity<String> createReview(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -34,6 +38,7 @@ public class ReviewController {
          return ResponseEntity.ok("리뷰가 성공적으로 등록 되었습니다.");
     }
 
+    @Operation(summary = "후기 수정", description = "작성된 후기를 수정합니다.")
     @PutMapping("/review/{reviewId}")
     public ResponseEntity<ReviewUpdateResponseDto> updateReview(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -44,6 +49,7 @@ public class ReviewController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "후기 삭제", description = "작성된 후기를 삭제합니다.")
     @PatchMapping("/review/{reviewId}")
     public ResponseEntity<ReviewUpdateResponseDto> deleteReview(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -53,6 +59,7 @@ public class ReviewController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "식당에 작성된 전체 후기 조회", description = "식당에 작성된 전체 후기를 조회합니다.")
     @GetMapping("/review/{restaurantId}")
     public ResponseEntity<PagedModel<ReviewInfoListDto>> reviewAll(
             @PathVariable UUID restaurantId,
