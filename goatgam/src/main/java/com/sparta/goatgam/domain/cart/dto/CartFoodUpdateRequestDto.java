159958654b7993fb5ca06b36131bf097384f69b3
River is line <dmstjs7047@hanmail.net>
2025-10-15 11:44:08 +0900

    docs : 증감옵션 스웨거 설명 추가

--- goatgam/src/main/java/com/sparta/goatgam/domain/cart/dto/CartFoodUpdateRequestDto.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/cart/dto/CartFoodUpdateRequestDto.java
@@ -1,6 +1,9 @@
 package com.sparta.goatgam.domain.cart.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CartFoodUpdateRequestDto(
+        @Schema(description = "증감 옵션입니다. \"INCREASE\" 또는 \"DECREASE\"를 입력하세요.", example = "INCREASE")
         QuantityUpdateTypeEnum updateType
 ) {
 }
