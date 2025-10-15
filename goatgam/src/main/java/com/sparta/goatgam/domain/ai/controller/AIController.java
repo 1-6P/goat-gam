e67be56c444619a86413bcc70824d0644c7355e6
hangw2130\hangw <hangw2130@gmail.com>
2025-10-15 09:48:13 +0900

    annotation 제거

--- goatgam/src/main/java/com/sparta/goatgam/domain/ai/controller/AIController.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/ai/controller/AIController.java
@@ -8,7 +8,6 @@ import com.sparta.goatgam.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
--- goatgam/src/main/java/com/sparta/goatgam/domain/owner/controller/OwnerController.java
+++ goatgam/src/main/java/com/sparta/goatgam/domain/owner/controller/OwnerController.java
@@ -8,7 +8,6 @@ import com.sparta.goatgam.global.dto.MessageAndIdResponseDto;
 import com.sparta.goatgam.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
