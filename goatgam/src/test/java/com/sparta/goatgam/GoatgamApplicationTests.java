029938e5c20abdf064abf324c0ddf9933b3563f1
kinhyunsu <kimhyunsoo0720@gmail.com>
2025-10-16 14:40:44 +0900

    주석 제거

--- goatgam/src/test/java/com/sparta/goatgam/GoatgamApplicationTests.java
+++ goatgam/src/test/java/com/sparta/goatgam/GoatgamApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.goatgam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 class GoatgamApplicationTests {
 
     @Test
